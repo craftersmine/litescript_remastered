c1bf01689da01bebeecefac0e9f3893c7cbb3eb3
craftersmine <rodion.morozov.01@gmail.com>
2016-06-15 20:07:46 +0600

    Compiler library project creation

---
+++ LiteScript/LiteScript_Compile_Library/Main/Compile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace craftersmine.LiteScript.Lib.Compiler.Main
+{
+    public sealed class Compiler
+    {
+        public static string Compile(string command, string args)
+        {
+
+        }
+    }
+}
