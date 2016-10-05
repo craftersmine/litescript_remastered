4ebc4d3417c8488cbd6592529932a2eeb7f1b5e0
craftersmine <rodion.morozov.01@gmail.com>
2016-10-05 19:36:50 +0700

    Started work on LiteScript Plugin Manager

---
+++ litescript_plugin_manager/Install.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace craftersmine.LiteScript.Ide.PluginManager
+{
+    public partial class Install : Form
+    {
+        public Install(string plgId)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ litescript_plugin_manager/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace craftersmine.LiteScript.Ide.PluginManager
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Dictionary<string, string> _args = new Dictionary<string, string>();
+            foreach(var arg in args)
+            {
+                string[] _arg = arg.Split('=');
+                if (_arg[0] == "-do")
+                    _args.Add("do", _arg[1]);
+                if (_arg[0] == "-id")
+                    _args.Add("id", _arg[1]);
+            }
+            if (_args["do"] != string.Empty && _args["id"] != string.Empty)
+            {
+                if (_args["do"].ToLower() == "install")
+                    Application.Run(new Install(_args["id"]));
+                if (_args["do"].ToLower() == "remove")
+                    Application.Run(new Remove(_args["id"]));
+            }
+        }
+    }
+}
