53dc99f5984da1e835b3bee443bf1f5890b015a8
Rodion <rodion.morozov.01@gmail.com>
2016-12-13 23:40:49 +0700

    Realized Plugin remover

--- litescript_plugin_manager/Remove.Designer.cs
+++ litescript_plugin_manager/Remove.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Remove));
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.status = new System.Windows.Forms.Label();
+            this.plugin_name = new System.Windows.Forms.Label();
+            this.tip = new System.Windows.Forms.Label();
+            this.cancel = new System.Windows.Forms.Button();
+            this.ok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(12, 289);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(426, 23);
+            this.progress.TabIndex = 11;
+            this.progress.Visible = false;
+            // 
+            // status
+            // 
+            this.status.Location = new System.Drawing.Point(12, 273);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(426, 13);
+            this.status.TabIndex = 10;
+            this.status.Text = "{status}";
+            this.status.Visible = false;
+            // 
+            // plugin_name
+            // 
+            this.plugin_name.AutoSize = true;
+            this.plugin_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.plugin_name.Location = new System.Drawing.Point(12, 44);
+            this.plugin_name.Name = "plugin_name";
+            this.plugin_name.Size = new System.Drawing.Size(88, 13);
+            this.plugin_name.TabIndex = 9;
+            this.plugin_name.Text = "{plugin_name}";
+            // 
+            // tip
+            // 
+            this.tip.Location = new System.Drawing.Point(12, 9);
+            this.tip.Name = "tip";
+            this.tip.Size = new System.Drawing.Size(426, 35);
+            this.tip.TabIndex = 8;
+            this.tip.Text = "{tip}";
+            // 
+            // cancel
+            // 
+            this.cancel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cancel.Location = new System.Drawing.Point(194, 371);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(110, 23);
+            this.cancel.TabIndex = 7;
+            this.cancel.Text = "{cancel}";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // ok
+            // 
+            this.ok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ok.Location = new System.Drawing.Point(310, 371);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(128, 23);
+            this.ok.TabIndex = 6;
+            this.ok.Text = "{ok}";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // Remove
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(450, 406);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.plugin_name);
+            this.Controls.Add(this.tip);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.ok);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Remove";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LiteScript Plugin Manager";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Remove_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Label plugin_name;
+        private System.Windows.Forms.Label tip;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button ok;
     }
 }
 
--- litescript_plugin_manager/Remove.cs
+++ litescript_plugin_manager/Remove.cs
@@ -7,14 +7,183 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Nini.Ini;
+using craftersmine.LocalizerLib;
 
 namespace craftersmine.LiteScript.Ide.PluginManager
 {
     public partial class Remove : Form
     {
+        private string pluginname = "";
+        private string[] reflibs = { };
+        private string mainlib = "";
+        private string _id;
+
+        public enum Stage { CollectingData, Removing, End, Cancel, Close }
+        public bool _isCloseNeeded = false;
+
+        private Stage _stg = Stage.CollectingData;
+        private LocalizationProvider _lprov = StaticData.LocaleProv;
+
+        private List<string> _files = new List<string>();
+        private string _resDir = "";
+        private string _metafile = "";
+
+        private string _l_finish;
+
+        private bool _isSuccessful = true;
+
         public Remove(string id)
         {
             InitializeComponent();
+
+            _id = id;
+
+            string metafile = Path.Combine(StaticData.PluginsDir, _id + "_pkgmeta.pkgmeta");
+            _metafile = metafile;
+
+            if (File.Exists(metafile))
+            {
+                IniDocument _ini = new IniDocument(metafile);
+                pluginname = _ini.Sections["Package"].GetValue("plugin-name");
+                mainlib = _ini.Sections["Package"].GetValue("plugin-lib");
+                reflibs = _ini.Sections["Package"].GetValue("referenced-libs").Split('|');
+
+                plugin_name.Text = pluginname;
+            }
+
+            tip.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.tip.start");
+            cancel.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.button.cancel");
+            ok.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.button.next");
+            _l_finish = _lprov.GetValue("app.pluginmanager.install-wizard.button.finish");
+        }
+
+        private void StageWorker()
+        {
+            switch (_stg)
+            {
+                case Stage.CollectingData:
+                    progress.Visible = true;
+                    status.Visible = true;
+                    ok.Enabled = false;
+                    cancel.Enabled = false;
+                    tip.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.tip.collecting-data");
+                    status.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.status.collecting-data");
+                    _files.Add(Path.Combine(StaticData.PluginsDir, mainlib));
+                    progress.Value = 10;
+                    foreach (var lib in reflibs)
+                    {
+                        _files.Add(Path.Combine(StaticData.PluginsDir, lib));
+                        progress.Value++;
+                    }
+                    _files.Add(_metafile);
+                    _resDir = Path.Combine(StaticData.PluginsDir, _id + "_Res");
+                    progress.Value += 15;
+                    _stg = Stage.Removing;
+                    StageWorker();
+                    break;
+                case Stage.Removing:
+                    
+                    foreach (var file in _files)
+                    {
+                        status.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.status.removing-file") + " " + Path.GetFileName(file);
+                        try
+                        {
+                            File.Delete(file);
+                        }
+                        catch
+                        {
+                            _stg = Stage.End;
+                            _isSuccessful = false;
+                            StageWorker();
+                        }
+                    }
+                    status.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.status.removing-res");
+                    try
+                    {
+                        Directory.Delete(_resDir, true);
+                        progress.Value += 35;
+                    }
+                    catch
+                    {
+                        _stg = Stage.End;
+                        _isSuccessful = false;
+                        StageWorker();
+                    }
+                    _stg = Stage.End;
+                    StageWorker();
+                    break;
+                case Stage.End:
+                    ok.Text = _l_finish;
+                    tip.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.tip.successful-completed");
+                    progress.Visible = false;
+                    ok.Enabled = false;
+                    try
+                    {
+                        Directory.Delete(StaticData.InstallerDir, true);
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    progress.Value = 80;
+                    progress.Visible = false;
+                    if (_isSuccessful)
+                        status.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.status.successful-completed");
+                    else status.Text = _lprov.GetValue("app.pluginmanager.remove-wizard.status.failed-remove");
+                    ok.Enabled = true;
+                    cancel.Visible = false;
+                    _isCloseNeeded = true;
+                    _stg = Stage.Close;
+                    break;
+                case Stage.Close:
+                    _isCloseNeeded = true;
+                    this.Close();
+                    break;
+                case Stage.Cancel:
+                    try
+                    {
+                        Directory.Delete(StaticData.InstallerDir, true);
+                    }
+                    catch { }
+                    this.Close();
+                    break;
+            }
+        }
+
+        private void Remove_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!_isCloseNeeded)
+            {
+                var dlg = MessageBox.Show(_lprov.GetValue("app.pluginmanager.remove-wizard.message.cancel-installation"), _lprov.GetValue("messages.titles.question"), MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                switch (dlg)
+                {
+                    case DialogResult.Yes:
+                        try
+                        {
+                            Directory.Delete(StaticData.InstallerDir);
+                        }
+                        catch
+                        { }
+                        Environment.Exit(0);
+                        break;
+                    case DialogResult.No:
+                        e.Cancel = true;
+                        break;
+                }
+            }
+            else Environment.Exit(0);
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            StageWorker();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
